5fe390d04d13bd70cbeefe9e3603daadbfed4bbc
Leon Antoniuc <antoniucleonwork@gmail.com>
2024-01-17 21:21:32 +0200

    inputfield emptyline fix

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     let buttonAdd = document.querySelector('.js-addBtn');
     let allTasks = document.querySelector('.tasks-list');
 
-    buttonAdd.addEventListener('click', function () {
+    buttonAdd.addEventListener('click', function () { 
         let inputFieldValue = document.querySelector('input').value;
-        let newTask = `<div class="task"><img src="img/check.svg" alt=""><div class="content"><p>${inputFieldValue}</p></div><img src="img/trash.svg" alt=""></div>`;
-        allTasks.innerHTML += newTask;
-        document.querySelector('input').value = ''; // Очистить поле ввода
+        let onlySpacesRegex = /^\s*$/; // Строка состоит только из пробелов
+
+        if (!onlySpacesRegex.test(inputFieldValue)) {
+            let newTask = `<div class="task"><img src="img/check.svg" alt=""><div class="content"><p>${inputFieldValue}</p></div><img src="img/trash.svg" alt=""></div>`;
+            allTasks.innerHTML += newTask;
+            document.querySelector('input').value = ''; // Очистить поле ввода
+        }
     });
 });
