901d92c891bda994205d532fcc9da87daa71258b
Leon Antoniuc <antoniucleonwork@gmail.com>
2024-01-17 20:50:31 +0200

    added js code to add tasks

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let buttonAdd = document.querySelector('.js-addBtn');
+    let allTasks = document.querySelector('.tasks-list');
+
+    buttonAdd.addEventListener('click', function () {
+        let inputFieldValue = document.querySelector('input').value;
+        let newTask = `<div class="task"><img src="img/check.svg" alt=""><p>${inputFieldValue}</p><img src="img/trash.svg" alt=""></div>`;
+        allTasks.innerHTML += newTask;
+        document.querySelector('input').value = ''; // Очистить поле ввода
+    });
+});
