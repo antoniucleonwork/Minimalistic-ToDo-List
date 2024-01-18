c774fdb09fca936c109ed053fcc84d8702b4f194
Leon Antoniuc <antoniucleonwork@gmail.com>
2024-01-18 20:07:57 +0200

    fixed js code bugs

--- js/script.js
+++ js/script.js
@@ -1,16 +1,43 @@
+function addTextToCrossOut(taskElement) {
+    let taskContent = taskElement.querySelector('.content p');
+    if (taskContent.innerHTML.startsWith("<del>")) {
+        taskContent.textContent = taskContent.textContent;
+    } else {
+        taskContent.innerHTML = "<del>" + taskContent.textContent + "</del>";
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     let buttonAdd = document.querySelector('.js-addBtn');
-    let allTasks = document.querySelector('.tasks-list');
+    let allTasks = document.querySelector('.tasks-list'); // check trash
 
-    buttonAdd.addEventListener('click', function () { 
+    buttonAdd.addEventListener('click', function () {
         let inputFieldValue = document.querySelector('input').value;
         let onlySpacesRegex = /^\s*$/; // Строка состоит только из пробелов
 
         if (!onlySpacesRegex.test(inputFieldValue)) {
-            let newTask = `<div class="task"><img src="img/check.svg" alt=""><div class="content"><p></p></div><img src="img/trash.svg" alt=""></div>`;
-            let task = inputFieldValue;
-            task.querySelector('p').textContent = task;
+            let newTask = document.createElement('div');
+            newTask.className = 'task';
+            newTask.innerHTML = `
+                <img src="img/check.svg" class="check">
+                <div class="content">
+                    <p></p>
+                </div>
+                <img src="img/trash.svg" class="trash">
+            `;
+
+            newTask.querySelector('.content p').textContent = inputFieldValue;
+
+            newTask.querySelector('.check').addEventListener('click', function () {
+                addTextToCrossOut(newTask);
+            });
+
+            newTask.querySelector('.trash').addEventListener('click', function () {
+                newTask.remove();
+            });
+
             document.querySelector('input').value = ''; // Очистить поле ввода
+            allTasks.appendChild(newTask); // Добавить новую задачу к списку
         }
     });
 });
